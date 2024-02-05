8a7e2108a35e093af02d21319b3e3194bd1b309f
alan <me@alansoto.info>
2024-02-05 17:15:21 +1000

    remove hardcoded project keys from sql query

--- chart-table-jdci-for-initiatives-query1.sql
+++ chart-table-jdci-for-initiatives-query1.sql
@@ -70,8 +70,6 @@ LEFT JOIN
 
 WHERE 
     `Issue`.`issue_type` = 'Initiative'
-    AND 
-    `PR`.`project_key` IN ('AUP', 'PANZ', 'UKPAY','INPAY')
 	-- Condition to filter the query using the calendar control
 	AND
 	`Issue`.`created_at` BETWEEN {CAL_CREATED_AT.START} AND {CAL_CREATED_AT.END}
