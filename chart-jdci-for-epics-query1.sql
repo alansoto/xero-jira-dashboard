623d964ca184cc53c3c60f688ad4f05859019b4c
alan <me@alansoto.info>
2024-02-05 20:09:24 +1000

    beautify and format code

--- chart-jdci-for-epics-query1.sql
+++ chart-jdci-for-epics-query1.sql
@@ -22,62 +22,62 @@ SELECT
 	
 FROM 
     `jira_issue` AS `Issue`
-INNER JOIN 
-	`jira_project` AS `PR` ON `PR`.`project_id` = `Issue`.`project_id`
--- Left join to bring Division OKR
-LEFT JOIN 
-    `jira_issue_field` AS `DOKR` 
-    ON `DOKR`.`issue_id` = `Issue`.`issue_id` AND `DOKR`.`name` = 'Division OKR'
--- Left join to bring Customer Impact
-LEFT JOIN 
-    `jira_issue_field` AS `CI` 
-    ON `CI`.`issue_id` = `Issue`.`issue_id` AND `CI`.`name` = 'Customer Impact'
--- Left join to bring Product FTE
-LEFT JOIN 
-    `jira_issue_field` AS `PFTE` 
-    ON `PFTE`.`issue_id` = `Issue`.`issue_id` AND `PFTE`.`name` = 'Product FTE'
--- Left join to bring Design FTE
-LEFT JOIN 
-    `jira_issue_field` AS `DFTE` 
-    ON `DFTE`.`issue_id` = `Issue`.`issue_id` AND `DFTE`.`name` = 'Design FTE'
--- Left join to bring Engineering FTE
-LEFT JOIN 
-    `jira_issue_field` AS `EFTE` 
-    ON `EFTE`.`issue_id` = `Issue`.`issue_id` AND `EFTE`.`name` = 'Engineering FTE'
--- Left join to bring L1 Metric
-LEFT JOIN 
-    `jira_issue_field` AS `L1M` 
-    ON `L1M`.`issue_id` = `Issue`.`issue_id` AND `L1M`.`name` = 'L1 Metric'
--- Left join to bring Channel Impact
-LEFT JOIN 
-    `jira_issue_field` AS `CIMP` 
-    ON `CIMP`.`issue_id` = `Issue`.`issue_id` AND `CIMP`.`name` = 'Channel Impact'
--- Left join to bring R&D
-LEFT JOIN 
-    `jira_issue_field` AS `R&D` 
-    ON `R&D`.`issue_id` = `Issue`.`issue_id` AND `R&D`.`name` = 'R&D'
--- Left join to bring RAG
-LEFT JOIN 
-    `jira_issue_field` AS `RAG` 
-    ON `RAG`.`issue_id` = `Issue`.`issue_id` AND `RAG`.`name` = 'RAG Status'
--- Left join to bring TOD
-LEFT JOIN 
-    `jira_issue_field` AS `TOD` 
-    ON `TOD`.`issue_id` = `Issue`.`issue_id` AND `TOD`.`name` = 'Taxonomy of Demand'
--- Left join to bring SD
-LEFT JOIN 
-    `jira_issue_field` AS `SD` 
-    ON `SD`.`issue_id` = `Issue`.`issue_id` AND `SD`.`name` = 'Start date'
--- Left join to bring TED
-LEFT JOIN 
-    `jira_issue_field` AS `TE` 
-    ON `TE`.`issue_id` = `Issue`.`issue_id` AND `TE`.`name` = 'Target End'
--- Left join to bring TN
-LEFT JOIN 
-    `jira_issue_field` AS `TEAM` 
-    ON `TEAM`.`issue_id` = `Issue`.`issue_id` AND `TEAM`.`name` = 'Assigned Team'
--- Left join to bring Parent
-LEFT JOIN `jira_issue` AS `Parent` ON `Issue`.`parent_issue_id` = `Parent`.`issue_id` 
+    INNER JOIN 
+        `jira_project` AS `PR` ON `PR`.`project_id` = `Issue`.`project_id`
+    -- Left join to bring Division OKR
+    LEFT JOIN 
+        `jira_issue_field` AS `DOKR` 
+        ON `DOKR`.`issue_id` = `Issue`.`issue_id` AND `DOKR`.`name` = 'Division OKR'
+    -- Left join to bring Customer Impact
+    LEFT JOIN 
+        `jira_issue_field` AS `CI` 
+        ON `CI`.`issue_id` = `Issue`.`issue_id` AND `CI`.`name` = 'Customer Impact'
+    -- Left join to bring Product FTE
+    LEFT JOIN 
+        `jira_issue_field` AS `PFTE` 
+        ON `PFTE`.`issue_id` = `Issue`.`issue_id` AND `PFTE`.`name` = 'Product FTE'
+    -- Left join to bring Design FTE
+    LEFT JOIN 
+        `jira_issue_field` AS `DFTE` 
+        ON `DFTE`.`issue_id` = `Issue`.`issue_id` AND `DFTE`.`name` = 'Design FTE'
+    -- Left join to bring Engineering FTE
+    LEFT JOIN 
+        `jira_issue_field` AS `EFTE` 
+        ON `EFTE`.`issue_id` = `Issue`.`issue_id` AND `EFTE`.`name` = 'Engineering FTE'
+    -- Left join to bring L1 Metric
+    LEFT JOIN 
+        `jira_issue_field` AS `L1M` 
+        ON `L1M`.`issue_id` = `Issue`.`issue_id` AND `L1M`.`name` = 'L1 Metric'
+    -- Left join to bring Channel Impact
+    LEFT JOIN 
+        `jira_issue_field` AS `CIMP` 
+        ON `CIMP`.`issue_id` = `Issue`.`issue_id` AND `CIMP`.`name` = 'Channel Impact'
+    -- Left join to bring R&D
+    LEFT JOIN 
+        `jira_issue_field` AS `R&D` 
+        ON `R&D`.`issue_id` = `Issue`.`issue_id` AND `R&D`.`name` = 'R&D'
+    -- Left join to bring RAG
+    LEFT JOIN 
+        `jira_issue_field` AS `RAG` 
+        ON `RAG`.`issue_id` = `Issue`.`issue_id` AND `RAG`.`name` = 'RAG Status'
+    -- Left join to bring TOD
+    LEFT JOIN 
+        `jira_issue_field` AS `TOD` 
+        ON `TOD`.`issue_id` = `Issue`.`issue_id` AND `TOD`.`name` = 'Taxonomy of Demand'
+    -- Left join to bring SD
+    LEFT JOIN 
+        `jira_issue_field` AS `SD` 
+        ON `SD`.`issue_id` = `Issue`.`issue_id` AND `SD`.`name` = 'Start date'
+    -- Left join to bring TED
+    LEFT JOIN 
+        `jira_issue_field` AS `TE` 
+        ON `TE`.`issue_id` = `Issue`.`issue_id` AND `TE`.`name` = 'Target End'
+    -- Left join to bring TN
+    LEFT JOIN 
+        `jira_issue_field` AS `TEAM` 
+        ON `TEAM`.`issue_id` = `Issue`.`issue_id` AND `TEAM`.`name` = 'Assigned Team'
+    -- Left join to bring Parent
+    LEFT JOIN `jira_issue` AS `Parent` ON `Issue`.`parent_issue_id` = `Parent`.`issue_id` 
 
 
 WHERE 
