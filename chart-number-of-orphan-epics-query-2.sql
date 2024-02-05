ee702509c0bf2debe809e15a6597fa89b32f2092
alan <me@alansoto.info>
2024-02-05 16:30:10 +1000

    add number of orphan epics chart

---
+++ chart-number-of-orphan-epics-query-2.sql
@@ -0,0 +1,6 @@
+SELECT "Subportfolios"."Project_Key" AS "Project Key"
+FROM "GoogleSheets"."Xero_subportfolios_Atlassian_Analytics_Data_Source_Subportfolios" AS "Subportfolios"
+WHERE {DROPDOWN_SUBPORTFOLIOS.IN('"Subportfolios"."Label"')}
+GROUP BY "Project Key"
+ORDER BY "Project Key" ASC
+LIMIT 1000;
