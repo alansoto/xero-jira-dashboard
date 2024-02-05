435afc59714c2bd7e2f4d0793ad2d6682adec9b7
alan <me@alansoto.info>
2024-02-05 19:31:14 +1000

    modify: add left joins to number of initiatives query

--- chart-number-of-initiatives-query1.sql
+++ chart-number-of-initiatives-query1.sql
@@ -3,8 +3,32 @@ SELECT
     COUNT(`Issue`.`issue_id`) AS `Issues`
 FROM
     `jira_issue` AS `Issue`
-INNER JOIN
-    `jira_project` AS `PR` ON `PR`.`project_id` = `Issue`.`project_id`
+    INNER JOIN `jira_project` AS `PR` 
+        ON `PR`.`project_id` = `Issue`.`project_id`
+    -- Left join to bring Company Objective
+    LEFT JOIN `jira_issue_field` AS `CO` 
+        ON `CO`.`issue_id` = `Issue`.`issue_id` AND `CO`.`name` = 'Company Objective'
+    -- Left join to bring Company Key Result
+    LEFT JOIN `jira_issue_field` AS `CKR` 
+        ON `CKR`.`issue_id` = `Issue`.`issue_id` AND `CKR`.`name` = 'Company Key Result'
+    -- Left join to bring Division OKR
+    LEFT JOIN `jira_issue_field` AS `DOKR` 
+        ON `DOKR`.`issue_id` = `Issue`.`issue_id` AND `DOKR`.`name` = 'Division OKR'
+    -- Left join to bring Customer Impact
+    LEFT JOIN `jira_issue_field` AS `CI` 
+        ON `CI`.`issue_id` = `Issue`.`issue_id` AND `CI`.`name` = 'Customer Impact'
+    -- Left join to bring L1 Metric
+    LEFT JOIN `jira_issue_field` AS `L1M` 
+        ON `L1M`.`issue_id` = `Issue`.`issue_id` AND `L1M`.`name` = 'L1 Metric'
+    -- Left join to bring Channel Impact
+    LEFT JOIN `jira_issue_field` AS `CIMP` 
+        ON `CIMP`.`issue_id` = `Issue`.`issue_id` AND `CIMP`.`name` = 'Channel Impact'
+    -- Left join to bring R&D
+    LEFT JOIN `jira_issue_field` AS `R&D` 
+        ON `R&D`.`issue_id` = `Issue`.`issue_id` AND `R&D`.`name` = 'R&D'
+    -- Left join to bring RAG Status
+    LEFT JOIN `jira_issue_field` AS `RAG` 
+        ON `RAG`.`issue_id` = `Issue`.`issue_id` AND `RAG`.`name` = 'RAG Status'
 WHERE
     `Issue`.`issue_type` = 'Initiative'
     -- Condition to filter the query using the calendar control
