315d0b9587b076ad450ff30216e18401299fd176
alan <me@alansoto.info>
2024-02-05 16:40:59 +1000

    add table - jdci for initiatives

---
+++ chart-table-jdci-for-initiatives-query1.sql
@@ -0,0 +1,89 @@
+SELECT 
+	`PR`.`project_key`,
+    `Issue`.`issue_key` AS `Issue key`,
+    `Issue`.`summary` AS `Summary`,
+	`Issue`.`created_at` AS `Created`,
+	`Issue`.`status` AS `Status`,
+	'https://xero.atlassian.net/browse/' ||  `Issue`.`issue_key` AS `URL`,
+	
+	-- Calculation of Jira Data Completeness Index
+    (
+        CASE WHEN `CKR`.`value` IS NULL THEN 0 ELSE 0.125 END +
+        CASE WHEN `CO`.`value` IS NULL THEN 0 ELSE 0.125 END +
+		CASE WHEN `DOKR`.`value` IS NULL THEN 0 ELSE 0.125 END +
+	  	CASE WHEN `CI`.`value` IS NULL THEN 0 ELSE 0.125 END +
+		CASE WHEN `L1M`.`value` IS NULL THEN 0 ELSE 0.125 END +
+		CASE WHEN `CIMP`.`value` IS NULL THEN 0 ELSE 0.125 END +
+	  	CASE WHEN `R&D`.`value` IS NULL THEN 0 ELSE 0.125 END +
+	  	CASE WHEN `RAG`.`value` IS NULL THEN 0 ELSE 0.125 END
+    )  AS `JDCI`,
+	
+	
+	
+    `CO`.`value` AS `Company Objective`,
+    `CKR`.`value` AS `Company Key Result`,
+	`DOKR`.`value` AS `Division OKR`,
+	`CI`.`value` AS `Customer Impact`,
+	`L1M`.`value` AS `L1 Metric`,
+	`CIMP`.`value` AS `Channel Impact`,
+	`R&D`.`value` AS `R&D`,
+	`RAG`.`value` AS `RAG Status`
+    
+    
+	
+FROM 
+    `jira_issue` AS `Issue`
+INNER JOIN 
+	`jira_project` AS `PR` ON `PR`.`project_id` = `Issue`.`project_id`
+-- Left join to bring Company Objective
+LEFT JOIN 
+    `jira_issue_field` AS `CO` 
+    ON `CO`.`issue_id` = `Issue`.`issue_id` AND `CO`.`name` = 'Company Objective'
+-- Left join to bring Company Key Result
+LEFT JOIN 
+    `jira_issue_field` AS `CKR` 
+    ON `CKR`.`issue_id` = `Issue`.`issue_id` AND `CKR`.`name` = 'Company Key Result'
+-- Left join to bring Division OKR
+LEFT JOIN 
+    `jira_issue_field` AS `DOKR` 
+    ON `DOKR`.`issue_id` = `Issue`.`issue_id` AND `DOKR`.`name` = 'Division OKR'
+-- Left join to bring Customer Impact
+LEFT JOIN 
+    `jira_issue_field` AS `CI` 
+    ON `CI`.`issue_id` = `Issue`.`issue_id` AND `CI`.`name` = 'Customer Impact'
+-- Left join to bring L1 Metric
+LEFT JOIN 
+    `jira_issue_field` AS `L1M` 
+    ON `L1M`.`issue_id` = `Issue`.`issue_id` AND `L1M`.`name` = 'L1 Metric'
+-- Left join to bring Channel Impact
+LEFT JOIN 
+    `jira_issue_field` AS `CIMP` 
+    ON `CIMP`.`issue_id` = `Issue`.`issue_id` AND `CIMP`.`name` = 'Channel Impact'
+-- Left join to bring R&D
+LEFT JOIN 
+    `jira_issue_field` AS `R&D` 
+    ON `R&D`.`issue_id` = `Issue`.`issue_id` AND `R&D`.`name` = 'R&D'
+-- Left join to bring RAG
+LEFT JOIN 
+    `jira_issue_field` AS `RAG` 
+    ON `RAG`.`issue_id` = `Issue`.`issue_id` AND `RAG`.`name` = 'RAG Status'
+
+WHERE 
+    `Issue`.`issue_type` = 'Initiative'
+    AND 
+    `PR`.`project_key` IN ('AUP', 'PANZ', 'UKPAY','INPAY')
+	-- Condition to filter the query using the calendar control
+	AND
+	`Issue`.`created_at` BETWEEN {CAL_CREATED_AT.START} AND {CAL_CREATED_AT.END}
+	-- Condition to filter the query using the dropdown status control
+	AND
+	{DROPDOWN_STATUS.IN('`Issue`.`status`')}
+	-- Condition to filter the query using the dropdown project control
+	AND
+	{DROPDOWN_PROJECT.IN('`PR`.`project_key`')}
+
+
+ORDER BY 
+    `Issue`.`issue_type` ASC,
+    `Issue`.`issue_key` ASC
+LIMIT 3000;
