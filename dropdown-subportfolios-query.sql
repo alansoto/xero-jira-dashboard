496e253b9c6ea64d102bd8758a3d8fec3e476bc6
alan <me@alansoto.info>
2024-02-05 20:29:14 +1000

    refactor: format code

--- dropdown-subportfolios-query.sql
+++ dropdown-subportfolios-query.sql
@@ -1,7 +1,13 @@
-SELECT "Subportfolios"."Label" AS "Subportfolio"
-FROM "GoogleSheets"."Xero_subportfolios_Atlassian_Analytics_Data_Source_Subportfolios" AS "Subportfolios"
-WHERE "Subportfolios"."Project_Key" IS NOT NULL
-AND "Subportfolios"."PoaP_fields_configured" = 'Yes'
-GROUP BY "Label"
-ORDER BY "Label" ASC
-LIMIT 1000;
+SELECT
+    "Subportfolios"."Label" AS "Subportfolio"
+FROM
+    "GoogleSheets"."Xero_subportfolios_Atlassian_Analytics_Data_Source_Subportfolios" AS "Subportfolios"
+WHERE
+    "Subportfolios"."Project_Key" IS NOT NULL
+    AND "Subportfolios"."PoaP_fields_configured" = 'Yes'
+GROUP BY
+    "Label"
+ORDER BY
+    "Label" ASC
+LIMIT
+    1000;
