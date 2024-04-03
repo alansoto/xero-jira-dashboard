87ac1ff504e1b6928f3208d96dace97c8173b2a1
alan <me@alansoto.info>
2024-04-03 11:15:54 +1000

    ADD: Modify all charts to use new 'Show_in_dashboard' column

--- chart-fte-for-epics-query2.sql
+++ chart-fte-for-epics-query2.sql
@@ -4,9 +4,11 @@ FROM
     "GoogleSheets"."Xero_subportfolios_Atlassian_Analytics_Data_Source_Subportfolios" AS "Subportfolios"
 WHERE
     { DROPDOWN_SUBPORTFOLIOS.IN('"Subportfolios"."Label"') }
+    AND 
+    "Subportfolios"."Show_in_dashboard" = 'Yes'
 GROUP BY
     "Project Key"
 ORDER BY
     "Project Key" ASC
 LIMIT
-    1000;
+    5000;
--- chart-number-of-orphan-epics-query-2.sql
+++ chart-number-of-orphan-epics-query-2.sql
@@ -1,6 +1,14 @@
-SELECT "Subportfolios"."Project_Key" AS "Project Key"
-FROM "GoogleSheets"."Xero_subportfolios_Atlassian_Analytics_Data_Source_Subportfolios" AS "Subportfolios"
-WHERE {DROPDOWN_SUBPORTFOLIOS.IN('"Subportfolios"."Label"')}
-GROUP BY "Project Key"
-ORDER BY "Project Key" ASC
-LIMIT 1000;
+SELECT
+    "Subportfolios"."Project_Key" AS "Project Key"
+FROM
+    "GoogleSheets"."Xero_subportfolios_Atlassian_Analytics_Data_Source_Subportfolios" AS "Subportfolios"
+WHERE
+    { DROPDOWN_SUBPORTFOLIOS.IN('"Subportfolios"."Label"') }
+    AND 
+    "Subportfolios"."Show_in_dashboard" = 'Yes'
+GROUP BY
+    "Project Key"
+ORDER BY
+    "Project Key" ASC
+LIMIT
+    5000;
