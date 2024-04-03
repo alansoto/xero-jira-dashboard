d091dbbbb690fb31218e413bea4bb0a2d9e284f1
alan <me@alansoto.info>
2024-04-03 10:55:18 +1000

    FIX: Better query for status dropdown query

--- dropdown-status-query.sql
+++ dropdown-status-query.sql
@@ -2,6 +2,4 @@ SELECT DISTINCT `jira_issue`.`status` AS `Status`
 FROM `jira_issue` 
 INNER JOIN `jira_project` ON `jira_issue`.`project_id` = `jira_project`.`project_id`
 WHERE 
-`jira_project`.`project_key` = 'AUP' 
-AND 
-`jira_issue`.`issue_type` = 'Initiative'
+`jira_issue`.`issue_type` IN ('Initiative','Epic')
