18665fb32b1a2c466bbb8feef1cfd956daef600a
alan <me@alansoto.info>
2024-04-03 11:04:22 +1000

    ADD: Modify query to use new column Show_in_dashboard

--- dropdown-project-query.sql
+++ dropdown-project-query.sql
@@ -1,7 +1,8 @@
 SELECT "Subportfolios"."Project_Key" AS "Project Key"
 FROM "GoogleSheets"."Xero_subportfolios_Atlassian_Analytics_Data_Source_Subportfolios" AS "Subportfolios"
 WHERE "Subportfolios"."Project_Key" IS NOT NULL
+  AND "Subportfolios"."Show_in_dashboard" = 'Yes'
   AND {DROPDOWN_SUBPORTFOLIOS.IN('"Subportfolios"."Label"')}
 GROUP BY "Project Key"
 ORDER BY "Project Key" ASC
-LIMIT 1000;
+LIMIT 5000;
